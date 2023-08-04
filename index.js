78c0d330700cda3b6bd68d43b72906e978f9a947
Maciej Pliszek <pliszekmaciej0@gmail.com>
2023-08-04 09:15:14 +0200

    Add: logic to erase button, Change: color prompt to color input field

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-/*
-    1. create a grid:
-        800px x 800px
-
-        
-    2. add event
-*/
+// UI
 const grid = document.querySelector('.grid');
 const btnErase = document.querySelector('.btnErase');
-
 const num = 16;
-let color = prompt('Chose color');
 
-for(let i = 0; i < num; i++){
+let color = document.getElementById('colorChoice');
+color.addEventListener('input', getColor)
+//
+
+// Grid generation
+for (let i = 0; i < num; i++) {
+    // creating collumns
     const col = document.createElement('div');
     grid.append(col);
     col.className = 'col';
 
-    console.log('creating col '+ i)
-
-    for(let j = 0; j < num; j++){
+    for (let j = 0; j < num; j++) {
+        // filling the collumns with rows (cells)
         const cell = document.createElement('div');
         cell.className = 'cell';
         col.append(cell);
-        cell.addEventListener('mouseover', function(){
+        // adding event listener for coloring to every cell
+        cell.addEventListener('mouseover', function () {
             cell.style.backgroundColor = color;
             cell.className += ' colored';
         });
-        
-
-        console.log('creating row ' + j)
-    }  
+    }
 }
 
-btnErase.addEventListener('click', () => document.querySelector('.colored').style.backgroundColor = 'white');
+
+// Erase feature
+btnErase.addEventListener('click', Erase);
+
+function getColor(){
+    let userInput = document.getElementById('colorChoice').value;
+    return color = userInput;
+}
+
+function Erase(){
+    const coloredCells = document.querySelectorAll('.cell.colored')
+    coloredCells.forEach(cell => {
+        cell.className = 'cell';
+        cell.style.backgroundColor = 'white';
+    });
+    return;
+}
